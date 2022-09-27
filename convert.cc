d231dde0b8ec2263eac52e87c973e127e00fe310
Olly Betts <olly@survex.com>
2022-09-27 14:43:28 +1300

    Actually parse LO version number
    Previously we used a string compare, but that will break once the
    major version reaches 10.

--- convert.cc
+++ convert.cc
@@ -1,6 +1,6 @@
 /* convert.cc - Convert documents using LibreOfficeKit
  *
- * Copyright (C) 2014,2015,2016,2018,2019 Olly Betts
+ * Copyright (C) 2014-2022 Olly Betts
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -11,6 +11,7 @@
 
 #include "convert.h"
 
+#include <climits>
 #include <cstdlib>
 #include <exception>
 #include <iostream>
@@ -62,6 +63,9 @@ get_lo_path()
     // e.g. /opt/libreoffice6.3/program
     DIR* opt = opendir("/opt");
     if (opt) {
+	// We require at least LibreOffice 4.3.
+	unsigned long best_major = 4;
+	unsigned long best_minor = 2;
 	static string best_rc;
 	struct dirent* d;
 	while ((d = readdir(opt))) {
@@ -76,17 +80,27 @@ get_lo_path()
 		continue;
 	    }
 
-	    string rc = "/opt/";
-	    rc += d->d_name;
-	    rc += "/program";
-	    if (stat((rc + "/versionrc").c_str(), &sb) != 0 || !S_ISREG(sb.st_mode)) {
-		continue;
-	    }
+	    char* p = d->d_name + strlen("libreoffice");
+	    unsigned long major = strtoul(p, &p, 10);
+	    if (major == ULONG_MAX) continue;
+	    unsigned long minor = 0;
+	    if (*p == '.') {
+		minor = strtoul(p + 1, &p, 10);
+		if (minor == ULONG_MAX) continue;
 
-	    // Use string compare for now - FIXME: this will need reworking
-	    // once Libreoffice hits major version 10.
-	    if (rc > best_rc && rc >= "/opt/libreoffice4.3/program") {
-		best_rc = std::move(rc);
+		string rc = "/opt/";
+		rc += d->d_name;
+		rc += "/program";
+		if (stat((rc + "/versionrc").c_str(), &sb) != 0 || !S_ISREG(sb.st_mode)) {
+		    continue;
+		}
+
+		if (major > best_major ||
+		    (major == best_major && minor > best_minor)) {
+		    best_major = major;
+		    best_minor = minor;
+		    best_rc = std::move(rc);
+		}
 	    }
 	}
 	closedir(opt);
